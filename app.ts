160fe078a1310e33d9dc86570937a4a44a32ed7b
travmonkey <travis@batk.me>
2025-04-02 13:43:27 -0600

    Remove right panel functionality

--- app.ts
+++ app.ts
@@ -5,7 +5,6 @@ import Bar from "./widget/bar/Bar";
 import Applauncher from "./widget/appLauncher/Applauncher";
 import NotificationPopups from "./widget/notifications/NotificationPopups";
 import LeftPanel from "./widget/leftPanel/LeftPanel";
-import RightPanel from "./widget/rightPanel/rightPanel";
 import OSD from "./widget/osd/osd";
 import wallpaperSwitcher from "./widget/wallpaperSwitcher/wallpaperSwitcher";
 import PowerMenu from "./widget/powermenu/powermenu";
@@ -29,7 +28,6 @@ App.start({
     App.get_monitors().map(Applauncher);
     App.get_monitors().map(NotificationPopups);
     App.get_monitors().map(LeftPanel);
-    App.get_monitors().map(RightPanel);
     App.get_monitors().map(OSD);
     App.get_monitors().map(wallpaperSwitcher);
     App.get_monitors().map(PowerMenu);
